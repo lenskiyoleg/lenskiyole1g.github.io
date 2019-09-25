8b9d0f3d6372c99ed78ce2acfafbb8c2186d8134
lenskiyoleg <55747728+lenskiyoleg@users.noreply.github.com>
2019-09-25 15:26:23 +0400

    Add files via upload

---
+++ src0/navbar.php
@@ -0,0 +1,17 @@
+<div class="container-fluid">
+ 		<nav class="navbar navbar-expand-lg navbar-light bg-light">
+   	<a class="navbar-brand" href="index.html">NOVENTA</a>
+   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item"><a class="nav-link peep" href="index.php">Home</a></li>
+      <li class="nav-item"><a class="nav-link peep" href="about.php">About</a></li>
+      <li class="nav-item"><a class="nav-link peep" href="photo.php">Photo</a></li>
+      <li class="nav-item"><a class="nav-link peep" href="contacts.php">Contacts</a></li>
+    </ul> 
+  </div>
+
+ 	</div>
